912878b9ef019a142acba02e8921a75ba96fc7b0
pragathish07 <pragarajesh779jd@gmail.com>
2024-12-02 21:51:08 +0530

    created ui components

--- client/src/pages/Builder.tsx
+++ client/src/pages/Builder.tsx
@@ -257,7 +257,7 @@ export function Builder() {
               {activeTab === 'code' ? (
                 <CodeEditor file={selectedFile} />
               ) : (
-                <PreviewFrame webContainer={webcontainer!} files={files} />
+                <PreviewFrame webContainer={webcontainer} files={files} />
               )}
             </div>
           </div>
